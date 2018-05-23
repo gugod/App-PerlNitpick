6df4325d05404e7c36202d877debe7b05df74516
Kang-min Liu <gugod@gugod.org>
2018-05-23 16:23:09 +0900

    About the word "SimpleStr"
    It is actually a type declared in https://metacpan.org/pod/MooseX::Types::Common::String

--- lib/App/P5Nitpick/Rule/QuoteSimpleStringWithSingleQuote.pm
+++ lib/App/P5Nitpick/Rule/QuoteSimpleStringWithSingleQuote.pm
@@ -1,17 +1,17 @@
-package App::P5Nitpick::Rule::QuoteSimpleStrWithSingleQuote;
+package App::P5Nitpick::Rule::QuoteSimpleStringWithSingleQuote;
 # ABSTRACT: Re-quote strings with single quotes ('') if they look "simple"
 
 =encoding UTF-8
 
 =head1 DESCRIPTION
 
-This nitpicking rule re-quote SimpleStr with single-quote.
+This nitpicking rule re-quote simple strings with single-quote.
 For example, C<"coffee"> becomes C<'coffee'>.
 
-=head2 SimpleStr ?
+=head2 Simple strings ?
 
-SimpleStr (simple strings) is a type of string literal that satisfy
-all of these constraints:
+Simple strings is a subset of strings that satisfies all of these
+constraints:
 
     - is a string literal (not variable)
     - is quoted with: q, qq, double-quote ("), or single-quote (')
@@ -38,5 +38,26 @@ Roughly speaking, given a string, if you can re-quote it with single-quote (')
 without changing its value -- then it is a simple string.
 
 =cut
+
+use Moose;
+use PPI::Document;
+
+has document => (
+    is => 'ro',
+    required => 1,
+    isa => 'PPI::Document',
+);
+
+sub rewrite {
+    my ($self) = @_;
+
+    my $doc = $self->document;
+    my $toks = $doc->find(sub { $_[1]->isa('PPI::Token::Quote::Double') });
+
+    $_->simplify for @$toks;
+
+    my $new_code = "$doc";
+    return PPI::Document->new( \$new_code );
+}
 
 1;
