c7a2f4c8decce881926d9d0d94b463e98597f19f
Kang-min Liu <gugod@gugod.org>
2018-06-05 21:18:16 +0900

    more blank rules

---
+++ lib/App/P5Nitpick/Rule/MoreOrLessHorizontalSpaces.pm
@@ -0,0 +1,12 @@
+package App::P5Nitpick::Rule::MoreOrLessHorizontalSpaces;
+
+
+1;
+
+__END__
+
+=head1 MoreOrLessHorizontalSpaces
+
+In this rule, space characters is inserted or removed within one line
+between punctuation boundaries.
+
