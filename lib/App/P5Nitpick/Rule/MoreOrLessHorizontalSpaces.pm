59e34a4170df830f34ae79e7246402f5d47f1fca
Kang-min Liu <gugod@gugod.org>
2018-06-19 17:28:07 +0900

    implement one trivial rewrite rule.
    In comma-seperated lists, in ensure that there is only one space
    character following the comma.
    
    Ex, it turns this:
    
       (1,2,    3)
    
    ... into:
    
       (1, 2, 3)

--- lib/App/P5Nitpick/Rule/MoreOrLessHorizontalSpaces.pm
+++ lib/App/P5Nitpick/Rule/MoreOrLessHorizontalSpaces.pm
@@ -1,5 +1,38 @@
-package App::P5Nitpick::Rule::MoreOrLessHorizontalSpaces;
+package App::P5Nitpick::Rule::MoreOrLessHorizontalSpaces ;
+use Moose;
+use PPI::Document;
+use PPI::Token::Whitespace;
 
+has document => (
+    is => 'ro',
+    required => 1,
+    isa => 'PPI::Document',
+);
+
+no Moose;
+
+sub rewrite {
+    my ($self) = @_;
+    my $doc = $self->document;
+    for my $el0 (@{ $doc->find('PPI::Structure::List') }) {
+        for my $el (@{ $el0->find('PPI::Token::Operator') }) {
+            next unless $el->content eq ',';
+            my $next_el = $el->next_sibling;
+            if ($next_el->isa('PPI::Token::Whitespace')) {
+                # Make sure there is only one whitespace
+                if ($next_el->content =~ /  +/) {
+                    $next_el->set_content(' ');
+                }
+            } else {
+                # Insert a new one.
+                my $wht = PPI::Token::Whitespace->new(' ');
+                $el->insert_after($wht);
+            }
+        }
+    }
+
+    return $self->document;
+}
 
 1;
 
