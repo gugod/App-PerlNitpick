e5704fc44df0d0be30a9eae78950ed9050b12094
Kang-min Liu <gugod@gugod.org>
2018-07-03 21:23:09 +0900

    RewriteWithAssigmentOperators: start with a naive implementation.
    This implementation does not trim spaces, but works already.

---
+++ lib/App/P5Nitpick/Rule/RewriteWithAssignmentOperators.pm
@@ -0,0 +1,60 @@
+package App::P5Nitpick::Rule::RewriteWithAssignmentOperators;
+
+use Moose;
+use PPI::Document;
+use PPI::Token::Whitespace;
+
+no Moose;
+
+sub rewrite {
+    my ($self, $document) = @_;
+
+    my @op_assign = grep {
+        my $op = $_;
+        $op->content eq '=' && $op->parent->isa('PPI::Statement') && $op->parent->schildren == 6 && do {
+            my $tok = $op->sprevious_sibling;
+            $tok->isa('PPI::Token::Symbol') && $tok->raw_type eq '$'
+        } 
+    } @{$document->find('PPI::Token::Operator') // []};
+    return $document unless @op_assign;
+
+    my @found;
+    for my $op (@op_assign) {
+        my $prev = $op->sprevious_sibling;
+        my $next = $op->snext_sibling;
+        if ($next->isa('PPI::Token::Symbol') && $prev->content eq $next->content) {
+            push @found, $op->parent;
+        }
+    }
+
+    for my $statement (@found) {
+        my @child = $statement->schildren;
+
+        # assigment operator :)
+        my $assop = PPI::Token::Operator->new($child[3]->content . $child[1]->content);
+
+        $child[3]->remove;
+        $child[2]->remove;
+        $child[1]->insert_after($assop);
+        $child[1]->remove;
+    }
+
+    return $document;
+}
+
+1;
+
+__END__
+
+=head1 DESCRIPTION
+
+This rule rewrites those assignments that alter a single varible with itself.
+For example, this one:
+
+    $x = $x + 2;
+
+Is rewritten with the C<+=> assignment operator, as:
+
+    $x += 2;
+
+=cut
---
+++ t/RewriteWithAssignmentOperators.t
@@ -0,0 +1,25 @@
+#!perl
+use strict;
+use Test2::V0;
+
+use PPI::Document;
+use App::P5Nitpick::Rule::RewriteWithAssignmentOperators;
+
+my @tests = (
+    ['$x=$x+2;', '$x+=2;'],
+    ['$x=$x*2;', '$x*=2;'],
+    ['$x=$x*$x;', '$x*=$x;'],
+    ['$x=$x//3;', '$x//=3;'],
+);
+
+for my $t (@tests) {
+    my ($code_before, $code_after) = @$t;
+
+    my $doc = PPI::Document->new(\$code_before);
+    my $o = App::P5Nitpick::Rule::RewriteWithAssignmentOperators->new();
+    my $doc2 = $o->rewrite($doc);
+    my $code2 = "$doc2";
+    is $code2, $code_after, $code_before;
+}
+
+done_testing;
