09c3f4dd105cb6a233cae283c18a3e0d095dc6a5
Kang-min Liu <gugod@gugod.org>
2018-08-23 13:54:06 +0900

    manually list all rules.
    this makes it a little bit more PAR-friendly.

--- lib/App/P5Nitpick/Nitpicker.pm
+++ lib/App/P5Nitpick/Nitpicker.pm
@@ -21,10 +21,24 @@ has inplace => (
 );
 
 use PPI::Document;
-use Module::Find qw(useall);
-use File::Slurp qw(read_file);
 
-my @rules = sort { $a cmp $b } useall App::P5Nitpick::Rule;
+# use Module::Find qw(useall);
+# my @rules = sort { $a cmp $b } useall App::P5Nitpick::Rule;
+
+# perl -Mlib=local -Ilib -MModule::Find=findallmod -E 'say "use $_;" for findallmod("App::P5Nitpick::Rule")'
+use App::P5Nitpick::Rule::AppendUnimportStatement;
+use App::P5Nitpick::Rule::DedupeIncludeStatements;
+use App::P5Nitpick::Rule::MoreOrLessSpaces;
+use App::P5Nitpick::Rule::QuoteSimpleStringWithSingleQuote;
+use App::P5Nitpick::Rule::RemoveEffectlessUTF8Pragma;
+use App::P5Nitpick::Rule::RemoveUnusedImport;
+use App::P5Nitpick::Rule::RemoveUnusedInclude;
+use App::P5Nitpick::Rule::RemoveUnusedVariables;
+use App::P5Nitpick::Rule::RewriteHeredocAsQuotedString;
+use App::P5Nitpick::Rule::RewriteRefWithRefUtil;
+use App::P5Nitpick::Rule::RewriteWithAssignmentOperators;
+
+my @rules = qw(AppendUnimportStatement DedupeIncludeStatements MoreOrLessSpaces QuoteSimpleStringWithSingleQuote RemoveEffectlessUTF8Pragma RemoveUnusedImport RemoveUnusedInclude RemoveUnusedVariables RewriteHeredocAsQuotedString RewriteRefWithRefUtil RewriteWithAssignmentOperators);
 
 sub list_rules {
     my ($class) = @_;
